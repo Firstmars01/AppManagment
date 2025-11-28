a6bec933fc75d001bf1830a562e78154cdcbddf1
MARIE Clément <clement.marie24@gmail.com>
2025-11-28 15:15:36 +0100

    add - dashboard

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProjectRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DashboardController extends AbstractController
+{
+    private ProjectRepository $projectRepository;
+
+    public function __construct(ProjectRepository $projectRepository)
+    {
+        $this->projectRepository = $projectRepository;
+    }
+
+    #[Route('/dashboard', name: 'app_dashboard')]
+    public function index(): Response
+    {
+        // Récupère l'utilisateur connecté
+        $user = $this->getUser();
+
+        if (!$user) {
+            throw $this->createAccessDeniedException("Vous devez être connecté.");
+        }
+
+        // Si ton entity Project a une propriété owner (ManyToOne(User))
+        $projects = $this->projectRepository->findBy(
+            ['owner' => $user],
+            ['id' => 'ASC'] // ou un autre tri
+        );
+
+        return $this->render('dashboard/index.html.twig', [
+            'projects' => $projects, // ⚠️ important de l’envoyer à Twig
+        ]);
+    }
+}
--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -6,7 +6,6 @@ use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Uid\Uuid;
 
 class ProjectController extends AbstractController
 {
