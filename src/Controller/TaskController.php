473f262a65cba4763025120006b7c6aebd3fbc33
MARIE Clément <clement.marie24@gmail.com>
2025-12-01 13:41:45 +0100

    add - every param is link

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,17 +2,54 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectRepository;
+use App\Repository\TaskRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class TaskController extends AbstractController
 {
-    #[Route('/task', name: 'app_task')]
-    public function index(): Response
+    private ProjectRepository $projectRepository;
+    private TaskRepository $taskRepository;
+
+    public function __construct(
+        ProjectRepository $projectRepository,
+        TaskRepository $taskRepository
+    ) {
+        $this->projectRepository = $projectRepository;
+        $this->taskRepository = $taskRepository;
+    }
+
+    #[Route('/project/{slug}/task/{id}', name: 'app_task')]
+    public function index(string $slug, string $id): Response
     {
+        $project = $this->projectRepository->findOneBy(['slug' => $slug]);
+
+        if (!$project) {
+            throw $this->createNotFoundException('Ce projet n\'existe pas');
+        }
+
+        $task = $this->taskRepository->find($id);
+
+        if (!$task) {
+            throw $this->createNotFoundException('Cette tâche n\'existe pas');
+        }
+        $taskBelongsToProject = false;
+
+        if ($task->getMilestone() && $task->getMilestone()->getProject() === $project) {
+            $taskBelongsToProject = true;
+        } elseif ($task->getRequirement() && $task->getRequirement()->getProject() === $project) {
+            $taskBelongsToProject = true;
+        }
+
+        if (!$taskBelongsToProject) {
+            throw $this->createNotFoundException('Cette tâche n\'appartient pas à ce projet');
+        }
+
         return $this->render('task/task.html.twig', [
-            'controller_name' => 'TaskController',
+            'project' => $project,
+            'task' => $task,
         ]);
     }
 }
