a25b2a8d26141826c928bc168d76b3f24d85f05c
MARIE Clément <clement.marie24@gmail.com>
2025-12-01 10:15:03 +0100

    update - show only milestone in project

--- src/Controller/MilestoneController.php
+++ src/Controller/MilestoneController.php
@@ -3,28 +3,47 @@
 namespace App\Controller;
 
 use App\Repository\ProjectRepository;
+use App\Repository\MilestoneRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class MilestoneController extends AbstractController
+class MilestoneController extends AbstractController
 {
     private ProjectRepository $projectRepository;
+    private MilestoneRepository $milestoneRepository;
 
-    public function __construct(ProjectRepository $projectRepository)
-    {
+    public function __construct(
+        ProjectRepository $projectRepository,
+        MilestoneRepository $milestoneRepository
+    ) {
         $this->projectRepository = $projectRepository;
-
+        $this->milestoneRepository = $milestoneRepository;
     }
 
     #[Route('/project/{slug}/milestone/{id}', name: 'app_milestone')]
     public function index(string $slug, string $id): Response
     {
         $project = $this->projectRepository->findOneBy(['slug' => $slug]);
-        $milestone = $this->projectRepository->findMilestoneById($id);
+
+        if (!$project) {
+            throw $this->createNotFoundException('Ce projet n\'existe pas');
+        }
+
+        $milestone = $this->milestoneRepository->find($id);
+
+        if (!$milestone) {
+            throw $this->createNotFoundException('Ce milestone n\'existe pas');
+        }
+
+        // Vérifier que le milestone appartient bien au projet
+        if ($milestone->getProject() !== $project) {
+            throw $this->createNotFoundException('Ce milestone n\'appartient pas à ce projet');
+        }
 
         return $this->render('milestone/index.html.twig', [
-            'controller_name' => 'MilestoneController',
+            'project' => $project,
+            'milestone' => $milestone,
         ]);
     }
 }
--- src/Controller/RequirementController.php
+++ src/Controller/RequirementController.php
@@ -2,17 +2,59 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectRepository;
+use App\Repository\MilestoneRepository;
+use App\Repository\RequirementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class RequirementController extends AbstractController
+class RequirementController extends AbstractController
 {
-    #[Route('/requirement', name: 'app_requirement')]
-    public function index(): Response
+    private ProjectRepository $projectRepository;
+    private MilestoneRepository $milestoneRepository;
+    private RequirementRepository $requirementRepository;
+
+    public function __construct(
+        ProjectRepository $projectRepository,
+        MilestoneRepository $milestoneRepository,
+        RequirementRepository $requirementRepository
+    ) {
+        $this->projectRepository = $projectRepository;
+        $this->milestoneRepository = $milestoneRepository;
+        $this->requirementRepository = $requirementRepository;
+    }
+
+    #[Route('/project/{slug}/milestone/{milestoneId}/requirement/{id}', name: 'app_requirement')]
+    public function index(string $slug, int $milestoneId, int $id): Response
     {
+        $project = $this->projectRepository->findOneBy(['slug' => $slug]);
+
+        if (!$project) {
+            throw $this->createNotFoundException('Ce projet n\'existe pas');
+        }
+
+        $milestone = $this->milestoneRepository->find($milestoneId);
+
+        if (!$milestone || $milestone->getProject() !== $project) {
+            throw $this->createNotFoundException('Ce milestone n\'existe pas');
+        }
+
+        $requirement = $this->requirementRepository->find($id);
+
+        if (!$requirement) {
+            throw $this->createNotFoundException('Ce requirement n\'existe pas');
+        }
+
+        // Vérifier que le requirement appartient bien au milestone
+        if ($requirement->getMilestone() !== $milestone) {
+            throw $this->createNotFoundException('Ce requirement n\'appartient pas à ce milestone');
+        }
+
         return $this->render('requirement/index.html.twig', [
-            'controller_name' => 'RequirementController',
+            'project' => $project,
+            'milestone' => $milestone,
+            'requirement' => $requirement,
         ]);
     }
 }
