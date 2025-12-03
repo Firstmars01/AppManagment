fcc849e0f8642f985ea775613309f893d696a643
MARIE Clément <clement.marie24@gmail.com>
2025-12-03 09:47:37 +0100

    add - crud for task

---
+++ src/Form/TaskType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Milestone;
+use App\Entity\Requirement;
+use App\Entity\Task;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('label')
+            ->add('isFunctional')
+            ->add('plannedStartDate', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('daysEstimate')
+            ->add('description')
+            ->add('milestone', EntityType::class, [
+                'class' => Milestone::class,
+                'choice_label' => 'label',
+            ])
+            ->add('manager', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => function (User $user) {
+                    return $user->getName() . ' ' . $user->getSecondName();
+                },
+            ])
+            ->add('previousTask', EntityType::class, [
+                'class' => Task::class,
+                'choice_label' => 'label',
+            ])
+            ->add('requirements', EntityType::class, [
+                'class' => Requirement::class,
+                'choice_label' => function(Requirement $requirement) {
+                    return $requirement->getDescription();
+                },
+                'multiple' => true,
+                'expanded' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
